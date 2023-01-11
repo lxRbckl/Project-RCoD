4e4b258ace79881f87a83b3d37ccc407b90a731f
lxRbckl <lxrbckl@gmail.com>
2023-01-11 15:53:14 -0600

    Initial commit

---
+++ main.py
@@ -0,0 +1,29 @@
+# Project RCoN by Alex Arbuckle #
+
+
+# import <
+from discord import Intents
+from discord.ext import commands
+from pyautogui import locateAllOnScreen
+
+
+# >
+
+
+# global <
+discordToken = ''
+
+gSettingLink = ''
+RCoD = commands.Bot(command_prefix = '', intents = Intents.all())
+
+
+# >
+
+
+@RCoD.command(aliases = )
+
+
+# main <
+if (__name__ == '__main__'): pass
+
+# >
