f4acccee6eb543abc0bcd49a10d3a9ac35a241d0
lxRbckl <lxrbckl@gmail.com>
2023-11-19 04:23:39 -0600

    xc

---
+++ source/screen.py
@@ -0,0 +1,32 @@
+# import <
+from pyautogui import (
+   
+   size,
+   click,
+   moveTo,
+   locateCenterOnScreen
+   
+)
+
+# >
+
+
+class screen:
+   
+   
+   def __init__():
+      '''  '''
+      
+      
+   
+   
+   def screenshot():
+      '''  '''
+      
+      pass
+   
+   
+   def click():
+      '''  '''
+      
+      pass
