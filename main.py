0d016136fc90833ad85401fe403887cd882ca06e
lxRbckl <lxrbckl@gmail.com>
2024-11-12 01:47:17 -0600

    Update

--- main.py
+++ main.py
@@ -23,15 +23,15 @@ if (__name__ == '__main__'):
       
       bot = Bot(
          
-         pRole = argv(1),
-         pQuery = argv(2),
-         pContact = argv(3),
-         pGuildId = argv(4),
-         pSkipQuery = argv(5),
-         pTokenOpenai = argv(6),
-         pMuteAfterCall = argv(8)
+         pRole = argv[1],
+         pQuery = argv[2],
+         pContact = argv[3],
+         pGuildId = argv[4],
+         pSkipQuery = argv[5],
+         pTokenOpenai = argv[6],
+         pMuteAfterCall = argv[8]
          
-      ).run(argv(7))
+      ).run(argv[7])
       
    except RuntimeError: pass
 
